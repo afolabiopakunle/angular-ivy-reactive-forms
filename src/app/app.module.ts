69ddd1e6f310c2573209afdd85bf1148834dd87e
afolabiopakunle <opakunleafolabi@gmail.com>
2021-11-03 08:42:24 +0100

    re-checked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, ],
+  imports:      [ BrowserModule, ReactiveFormsModule, ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
