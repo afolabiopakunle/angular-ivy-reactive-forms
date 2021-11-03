6353f5fe15774c4fb4f5709178d778ea69a92eba
afolabiopakunle <opakunleafolabi@gmail.com>
2021-11-03 08:38:13 +0100

    reactive forms module added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -6,6 +7,6 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  
+  myForm: FormGroup;
   
 }
