258c801702a13b43ba6f6800d72a512a58ef81b4
afolabiopakunle <opakunleafolabi@gmail.com>
2021-11-03 09:00:24 +0100

    reactive form works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component, VERSION } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   myForm: FormGroup;
   
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl()
+    })
+  }
+
+  save() {
+    console.log(this.myForm.value)
+  }
 }
